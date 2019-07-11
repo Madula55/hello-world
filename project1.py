260c065947dee52f34e7056ca2dd383dc1a5f406
Madula55 <andani.alex@gmail.com>
2019-07-11 23:56:45 +0200

    adding first comit in local rep

---
+++ project1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+# lets start
+x=np/linspace(0,10,100)
+y=x
+
+#plotting
+plt.plot(x,y)
+plt.xlabel("x")
+plt.ylabel("y")
+#end1
